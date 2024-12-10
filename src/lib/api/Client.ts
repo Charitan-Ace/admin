665e92f2aeff46bc88aa38ef537e2a94489c1fda
Nguyen Do <khoanguyen.do@outlook.com>
2024-12-11 01:09:44 +0700

    feat(api): differentiate post and get requests

--- src/lib/api/Client.ts
+++ src/lib/api/Client.ts
@@ -13,10 +13,31 @@ export default class APIClient {
   }
 
   // handle middleware/before-request here
-  async send<T>(path: string, options?: RequestInit) {
+  async get<T>(path: string, options?: RequestInit) {
     const url = new URL(path, this.baseURL);
 
-    const response = await fetch(url, options);
+    const response = await fetch(url, {
+      ...options,
+      method: "GET",
+    });
+
+    // TODO: standardized error class
+    if (!response.ok || response.status >= 400) {
+      throw new Error("Client error occurred");
+    }
+
+    const data = await response.json();
+
+    return data as T;
+  }
+
+  async post<T>(path: string, options?: RequestInit) {
+    const url = new URL(path, this.baseURL);
+
+    const response = await fetch(url, {
+      ...options,
+      method: "POST",
+    });
 
     // TODO: standardized error class
     if (!response.ok || response.status >= 400) {
