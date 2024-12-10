dae4ce6ff5c3fa3bab5445c40c4f33adcb9d15df
Nguyen Do <khoanguyen.do@outlook.com>
2024-12-10 17:29:47 +0700

    build: downgrade tailwind, add shadcn/ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,6 +10,10 @@ export default defineConfig({
         plugins: [["babel-plugin-react-compiler"]],
       },
     }),
-    tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
