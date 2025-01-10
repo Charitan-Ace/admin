5706c310216854abc624a3a1c618072db0b330a7
Potemkin Pavel <specialforbtsu@gmail.com>
2025-01-10 17:26:50 +0700

    add form input omponent

---
+++ src/components/reusable/form/input/FormInput.tsx
@@ -0,0 +1,30 @@
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { FormInputProps } from "./interfaces";
+import { FieldValues } from "react-hook-form";
+
+const FormInput = <T extends FieldValues>({
+  id,
+  type,
+  placeholder,
+  label,
+  error,
+  register,
+  disabled = false,
+  vSpacing = 2,
+  className = "",
+}: FormInputProps<T>) => (
+  <div className={`space-y-${vSpacing} ${className}`}>
+    <Label htmlFor={id}>{label}</Label>
+    <Input
+      id={id}
+      type={type}
+      placeholder={placeholder}
+      disabled={disabled}
+      {...register(id)}
+    />
+    {error && <p className="text-red-500 text-sm">{error}</p>}
+  </div>
+);
+
+export default FormInput;
